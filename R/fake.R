426fd0e87ae17b5b565bc22d139070a4d7fc858f
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-06-03 16:54:38 +0800

    Added packages

--- R/fake.R
+++ R/fake.R
@@ -1,3 +1,13 @@
+#' @import jeksterslabRsem
+#' @import jeksterslabRlinreg
+#' @import jeksterslabRdist
+#' @import jeksterslabRdoc
+#' @import jeksterslabRlib
+#' @import jeksterslabRnotes
+#' @import jeksterslabRmatrix
+#' @import jeksterslabRpkg
+#' @import jeksterslabRterm
+#' @import jeksterslabRutils
 fake <- function() {
   1 + 1
 }
